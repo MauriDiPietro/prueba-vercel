aa27bfa73734aa23b55754a14dfd273146c72a4e
MauriDiPietro <dipietro.jm@gmail.com>
2024-09-12 08:55:24 -0300

    dfdsf

--- api/index.js
+++ api/index.js
@@ -1,14 +1,2 @@
-import express from "express";
-import productRouter from '../src/routes/product.router.js'
-const app = express();
-
-app.get("/", (req, res) => res.send("Express on Vercel"));
-
-app.use(express.json());
-app.use(express.urlencoded({ extended:true }));
-
-app.use('/products', productRouter);
-
-app.listen(3000, () => console.log("Server ready on port 3000."));
-
+import app from "../src/server.js";
 export default app;
